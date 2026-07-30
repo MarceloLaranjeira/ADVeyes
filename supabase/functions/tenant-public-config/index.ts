72859925e1a5cd2e0ae179cda3ecf74e0e70dd16
MarceloLaranjeira <marcelolaranjeira33@gmail.com>
2026-07-29 23:16:30 -0400

    fix: liberar chamada pública de configuração

--- supabase/functions/tenant-public-config/index.ts
+++ supabase/functions/tenant-public-config/index.ts
@@ -6,7 +6,8 @@ import {
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, apikey, content-type",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
 };
 
